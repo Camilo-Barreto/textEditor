7547db525f1cada6deecc3875d778e1da17e77db
Camilo Barreto# <csbarreto11@gmail.com>
2024-06-30 19:11:31 +0100

    Implemented Page Up and Page Down features to move cursor to the top and bottom of the page

--- kilo.c
+++ kilo.c
@@ -22,7 +22,9 @@ enum editorKey {
 	ARROW_LEFT = 1000,
   	ARROW_RIGHT,
   	ARROW_UP,
-  	ARROW_DOWN
+  	ARROW_DOWN,
+	PAGE_UP,
+	PAGE_DOWN
 };
 
 /*** data ***/
@@ -102,11 +104,24 @@ int editorReadKey() {
 
 		// if the esc sequence is an arrow key esc sequence, return the corresponding wasd character
 		if (seq[0] == '[') {
-			switch (seq[1]) {
-				case 'A': return ARROW_UP;
-				case 'B': return ARROW_DOWN;
-				case 'C': return ARROW_RIGHT;
-				case 'D': return ARROW_LEFT;
+
+			// if the byte after [ is a digit we test if the 2nd char is ~. Then we test if the number before it was a 5 or a 6
+			if (seq[1] >= '0' && seq[1] <= '9') {
+				if (read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
+				if (seq[2] == '~') {
+					switch (seq[1]) {
+						case '5': return PAGE_UP;
+						case '6': return PAGE_DOWN;
+					}
+				}
+			}
+			else {
+				switch (seq[1]) {
+					case 'A': return ARROW_UP;
+					case 'B': return ARROW_DOWN;
+					case 'C': return ARROW_RIGHT;
+					case 'D': return ARROW_LEFT;
+				}
 			}
 		}
 		// if the esc char is not recognised return the escape character
@@ -314,6 +329,17 @@ void editorProcessKeypress() {
 			// Move cursor to the top left
 			write(STDOUT_FILENO, "\x1b[H", 3);
 			exit(0);
+			break;
+
+		case PAGE_UP:
+		case PAGE_DOWN:
+			// The following code block declares the time variable. To move the cursor we simulate the user pressing the up and down keys multiple time to send the cursor to the bottom or the top of the page. 
+			{
+				int times = E.screenrows;
+				while (times--) {
+					editorMoveCursor(c == PAGE_UP ? ARROW_UP: ARROW_DOWN);
+				}
+			}
 			break;
 		
 		case ARROW_UP:
